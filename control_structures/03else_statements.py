befc869b83e62602b284731b51246ccae6fd7120
Júlio Amorim <jclamorim@debian>
2018-03-13 09:08:20 -0300

    Ajuste na estrutura de pastas/assuntos

---
+++ control_structures/03else_statements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#example 1
+x = 4
+
+if x == 4:
+	print("Yes")
+else:
+	print("No")
+
+
+#example 2
+#only showing th eknowledge with conditions
+if 1+1 == 2:
+	if 2*2 == 8:
+		print("if")
+	else:
+		print("Else")
+
+
