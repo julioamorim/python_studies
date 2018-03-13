0349ce86e0ef0e7c12547ad34f471ec042a56ec2
Júlio Amorim <jclamorim@debian>
2018-03-13 11:04:38 -0300

    Boolean logic 100%

---
+++ control_structures/04boolean_logic.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#example 1
+print("===Example 1===")
+
+if(1 == 1) and (2 + 2 > 3):
+	print("true")
+else:
+	print("false")
+
+
+#example 2
+print("===Example 2===")
+
+age = 15
+money = 500
+if age > 18 or money > 100:
+	print("Welcome!")
+
+
+#example 3
+print("===Example 3===")
+
+if not True:
+	print("1")
+elif not(1 + 1 == 3):
+	print("2")
+else: print("3")
