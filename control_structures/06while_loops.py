2cf73051d47fe7ee99cfeca02cbb03c36f471c0d
Júlio Amorim <jclamorim@debian>
2018-03-16 15:08:23 -0300

    While loops statements added and well represeted

---
+++ control_structures/06while_loops.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#example 1
+i = 1
+while i <=5:
+	print(i)
+	i += 1
+
+print("Finished")
+
+#example 2
+
+
+number = 1
+
+while 1 == 1:
+	
+	print("in the look. interaction ")
+	break
+
+
+	#xample 3
+
+i = 0
+while 1==1:
+  print(i)
+  i = i + 1
+  if i >= 5:
+    print("Breaking")
+    break
+
+print("Finished")
