b6785de246afda1c8c5bb8f4e501e32b832b1268
Júlio Amorim <jclamorim@debian>
2018-03-16 10:30:36 -0300

    Example perators preedence added. Explain of th e symbols applyed in th elanguage too

---
+++ control_structures/05operator_precedence.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#example 1
+
+if 1 + 1 * 3 == 6:
+	print("Yes")
+else:
+	print("No")
+
+#operators
+# ** Exponentiation
+# ~ + - Complemetn, unary plus an dminus(method names for the last two are +@ and -@)
+# * / % // Multiuply, divide, modulo and floor division
+# = %= /= //= -= += *= **= assignment operators
+# is is not identity operators
+# in not in Membership operators
+# not or and Logical operators
+
+print("==========EXAMPLE 2===========\n")
+#example 2
+
+x = 4
+y = 2
+if not 1 + 1 == y or x == 4 and 7 == 8:
+	print("Yes")
+elif x > y:
+	print("No")
